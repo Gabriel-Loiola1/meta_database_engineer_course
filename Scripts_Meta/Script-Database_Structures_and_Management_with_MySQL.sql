a94f0bd049869e254cdf84d83e6391eb396ca8b5
Gabriel Loiola <jgabriel.loiola@gmail.com>
2026-05-07 16:33:52 -0300

    lab 2, modulo 1

--- Scripts_Meta/Script-Database_Structures_and_Management_with_MySQL.sql
+++ Scripts_Meta/Script-Database_Structures_and_Management_with_MySQL.sql
@@ -53,3 +53,51 @@ SELECT * FROM Employees WHERE NOT AnnualSalary > 50000;
 SELECT * FROM Employees WHERE Department IN ("Marketing", "Finance", "Legal") AND AnnualSalary < 50000; 
 SELECT * FROM Employees WHERE AnnualSalary BETWEEN 10000 AND 50000;
 SELECT * FROM Employees WHERE EmployeeName LIKE "S___%";
+
+show tables;
+
+-- Lab 1 | Módulo 1
+CREATE DATABASE little_lemon;
+USE little_lemon;
+
+CREATE TABLE Customers(
+	CustomerID INT NOT NULL PRIMARY KEY,
+	FullName VARCHAR(100) NOT NULL,
+	PhoneNumber INT NOT NULL UNIQUE
+);
+
+INSERT INTO Customers VALUES
+(1, "Vanessa McCarthy", 0757536378),
+(2, "Marcos Romero", 0757536379),
+(3, "Hiroki Yamane", 0757536376),
+(4, "Anna Iversen", 0757536375),
+(5, "Diana Pinto", 0757536374);
+
+CREATE TABLE Bookings(
+	BookingID INT NOT NULL PRIMARY KEY,
+	BookingDate DATE NOT NULL,
+	TableNumber INT NOT NULL,
+	NumberOfGuests INT NOT NULL CHECK (NumberOfGuests <= 8),
+	CustomerID INT NOT NULL,
+	FOREIGN KEY (CustomerID) REFERENCES Customers (CustomerID)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
+
+INSERT INTO Bookings VALUES
+(10, '2021-11-11', 7, 5, 1),
+(11, '2021-11-10', 5, 2, 2),
+(12, '2021-11-10', 3, 2, 4);
+
+-- Task 1
+SELECT c.FullName, c.PhoneNumber, b.BookingDate, b.NumberOfGuests
+FROM Customers AS c
+INNER JOIN Bookings as b
+ON c.CustomerID = b.CustomerID;
+
+-- Task 2
+SELECT c.CustomerID, b.BookingID
+FROM Customers AS c
+LEFT JOIN Bookings AS b
+ON c.CustomerID = b.CustomerID;
+
