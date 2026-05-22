eb3a9834a9af1e61e29120f0157ca0cd182021e4
Gabriel Loiola <jgabriel.loiola@gmail.com>
2026-05-22 19:11:24 -0300

    terminando módulo 2

--- Scripts_Meta/Script-Database_Structures_and_Management_with_MySQL.sql
+++ Scripts_Meta/Script-Database_Structures_and_Management_with_MySQL.sql
@@ -28,7 +28,7 @@ SELECT * FROM Orders WHERE Cost BETWEEN 50 AND 750;
 SELECT * FROM Orders WHERE ClientID = "Cl3" AND Cost > 100;
 SELECT * FROM Orders WHERE ProductID IN ("P1", "P2") AND Quantity > 2;
 
--- Reading Exercise | Módulo 1
+-- Reading Exercise 1 | Módulo 1
 
 CREATE TABLE Employees (
 	EmployeeID INT NOT NULL PRIMARY KEY,
@@ -56,7 +56,7 @@ SELECT * FROM Employees WHERE EmployeeName LIKE "S___%";
 
 show tables;
 
--- Lab 1 | Módulo 1
+-- Lab 2 | Módulo 1
 CREATE DATABASE little_lemon;
 USE little_lemon;
 
@@ -101,3 +101,105 @@ FROM Customers AS c
 LEFT JOIN Bookings AS b
 ON c.CustomerID = b.CustomerID;
 
+-- Lab 3 | Módulo 1
+DROP TABLE Orders;
+
+CREATE TABLE Orders(
+	OrderID INT,
+	Department VARCHAR(100),
+	OrderDate DATE,
+	OrderQty INT,
+	OrderTotal INT,
+	PRIMARY KEY(OrderID)
+);
+
+INSERT INTO Orders VALUES
+(1,'Lawn Care','2022-05-05',12,500),
+(2,'Decking','2022-05-22',150,1450),
+(3,'Compost and Stones','2022-05-27',20,780),
+(4,'Trees and Shrubs','2022-06-01',15,400),
+(5,'Garden Decor','2022-06-10',2,1250),
+(6,'Lawn Care','2022-06-10',12,500),
+(7,'Decking','2022-06-25',150,1450),
+(8,'Compost and Stones','2022-05-29',20,780),
+(9,'Trees and Shrubs','2022-06-10',15,400),
+(10,'Garden Decor','2022-06-10',2,1250),
+(11,'Lawn Care','2022-06-25',10,400), 
+(12,'Decking','2022-06-25',100,1400),
+(13,'Compost and Stones','2022-05-30',15,700),
+(14,'Trees and Shrubs','2022-06-15',10,300),
+(15,'Garden Decor','2022-06-11',2,1250),
+(16,'Lawn Care','2022-06-10',12,500),
+(17,'Decking','2022-06-25',150,1450),
+(18,'Trees and Shrubs','2022-06-10',15,400),
+(19,'Lawn Care','2022-06-10',12,500),
+(20,'Decking','2022-06-25',150,1450),
+(21,'Decking','2022-06-25',150,1450);
+
+-- Task 1 
+SELECT OrderDate FROM Orders GROUP BY OrderDate;
+
+-- Task 2
+SELECT OrderDate, COUNT(OrderID) FROM Orders GROUP BY OrderDate;
+
+-- Task 3
+SELECT Department, SUM(OrderQty) FROM Orders GROUP BY Department;
+
+-- Task 4
+SELECT OrderDate, COUNT(OrderID) FROM Orders
+GROUP BY OrderDate
+HAVING OrderDate BETWEEN '2022-06-01' AND '2022-06-30';
+
+-- Reading Exercise 2 | Módulo 1
+CREATE TABLE Employees_Orders (
+	OrderID INT NOT NULL,
+	EmployeeID INT NOT NULL,
+	Status VARCHAR(150),
+	HandlingCost INT DEFAULT NULL,
+	PRIMARY KEY (EmployeeID, OrderID),
+	FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+	FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO Employees_Orders VALUES
+(1,3,"In Progress",200), 
+(1,5,"Not Recieved",300), 
+(1,4,"Not Recieved",250), 
+(2,3,"Completed",200), 
+(2,5,"Completed",300), 
+(2,4,"In Progress",250), 
+(3,3,"In Progress",200), 
+(3,5,"Not Recieved",300), 
+(3,4,"Not Recieved",250), 
+(4,3,"Completed",200), 
+(4,5,"In Progress",300), 
+(4,4,"In Progress",250), 
+(5,3,"Completed",200), 
+(5,5,"In Progress",300), 
+(5,4,"Not Recieved",250), 
+(11,3,"Completed",200), 
+(11,5,"Completed",300), 
+(11,4,"Not Recieved",250), 
+(14,3,"Completed",200), 
+(14,5,"Not Recieved",300), 
+(14,4,"Not Recieved",250);
+
+-- Task 1
+SELECT EmployeeID, EmployeeName
+FROM Employees
+WHERE EmployeeID = ANY(SELECT EmployeeID FROM Employees_Orders WHERE Status = 'Completed');
+
+-- Task 2
+SELECT EmployeeID, HandlingCost
+FROM Employees_Orders
+WHERE HandlingCost > ALL(SELECT ROUND(OrderTotal / 100 * 20) FROM Orders);
+
+-- Task 3
+SELECT EmployeeID, HandlingCost
+FROM Employees_Orders
+WHERE HandlingCost > ALL(SELECT ROUND(OrderTotal / 100 * 20) FROM Orders) GROUP BY EmployeeID, HandlingCost;
+
+-- Task 4
+SELECT EmployeeID, HandlingCost
+FROM Employees_Orders
+WHERE HandlingCost > ALL(SELECT ROUND(OrderTotal / 100 * 20) AS twentyPercent FROM Orders GROUP BY OrderTotal HAVING twentyPercent > 100) GROUP BY EmployeeID, HandlingCost;
